835146df498a338eb1335560af8fb554b5a33ddc
Андрей Мельников <a.melnikov4@g.nsu.ru>
2025-05-25 14:50:34 +0700

    Rewrite library functions on my own
    Without copying licensed code

--- llvm/test_cdm/libs/inc/ctype.h
+++ llvm/test_cdm/libs/inc/ctype.h
@@ -1,18 +1,6 @@
 #ifndef F0BAB941_28DE_4845_9BFD_E4A49AA7FF61
 #define F0BAB941_28DE_4845_9BFD_E4A49AA7FF61
 
-int isblank(int c);
-// int isspace(int c);
-int islower(int c);
-int isupper(int c);
-int isdigit(int c);
-int isalpha(int c);
-int isalnum(int c);
-int isxdigit(int c);
-int isgraph(int c);
-int iscntrl(int c);
-int isprint(int c);
-int ispunct(int c);
 int tolower(int c);
 int toupper(int c);
 
--- llvm/test_cdm/libs/inc/string.h
+++ llvm/test_cdm/libs/inc/string.h
@@ -2,247 +2,86 @@
 #define FB7EC850_A287_4745_A5C1_2DA599F2D018
 
 #include "stddef.h"
-#include "stdint.h"
-
-/**
- * memcpy - Copy one area of memory to another
- * @dest: Where to copy to
- * @src: Where to copy from
- * @count: The size of the area.
- *
- * You should not use this function to access IO space, use memcpy_toio()
- * or memcpy_fromio() instead.
- */
-void *memcpy(void *dest, const void *src, size_t count);
-
-size_t strlen(const char *s);
 
 
 /**
- * strncasecmp - Case insensitive, length-limited string comparison
- * @s1: One string
- * @s2: The other string
- * @len: the maximum number of characters to compare
+ * Searches for the first occurrence of the character c (an unsigned char) in
+ * the first n bytes of the string pointed to, by the argument str.
  */
-int strncasecmp(const char *s1, const char *s2, size_t len);
-
-int strcasecmp(const char *s1, const char *s2);
-char *strcpy(char *dest, const char *src);
-char *strncpy(char *dest, const char *src, size_t count);
-/**
- * stpcpy - copy a string from src to dest returning a pointer to the new end
- *          of dest, including src's %NUL-terminator. May overrun dest.
- * @dest: pointer to end of string being copied into. Must be large enough
- *        to receive copy.
- * @src: pointer to the beginning of string being copied from. Must not overlap
- *       dest.
- *
- * stpcpy differs from strcpy in a key way: the return value is a pointer
- * to the new %NUL-terminating character in @dest. (For strcpy, the return
- * value is a pointer to the start of @dest). This interface is considered
- * unsafe as it doesn't perform bounds checking of the inputs. As such it's
- * not recommended for usage. Instead, its definition is provided in case
- * the compiler lowers other libcalls to stpcpy.
- */
-char *stpcpy(char *__restrict__ dest, const char *__restrict__ src);
-char *strcat(char *dest, const char *src);
-char *strncat(char *dest, const char *src, size_t count);
-size_t strlcat(char *dest, const char *src, size_t count);
-/**
- * strcmp - Compare two strings
- * @cs: One string
- * @ct: Another string
- */
-int strcmp(const char *cs, const char *ct);
-/**
- * strncmp - Compare two length-limited strings
- * @cs: One string
- * @ct: Another string
- * @count: The maximum number of bytes to compare
- */
-int strncmp(const char *cs, const char *ct, size_t count);
-/**
- * strchr - Find the first occurrence of a character in a string
- * @s: The string to be searched
- * @c: The character to search for
- *
- * Note that the %NUL-terminator is considered part of the string, and can
- * be searched for.
- */
-char *strchr(const char *s, int c);
-/**
- * strchrnul - Find and return a character in a string, or end of string
- * @s: The string to be searched
- * @c: The character to search for
- *
- * Returns pointer to first occurrence of 'c' in s. If c is not found, then
- * return a pointer to the null byte at the end of s.
- */
-char *strchrnul(const char *s, int c);
-/**
- * strnchrnul - Find and return a character in a length limited string,
- * or end of string
- * @s: The string to be searched
- * @count: The number of characters to be searched
- * @c: The character to search for
- *
- * Returns pointer to the first occurrence of 'c' in s. If c is not found,
- * then return a pointer to the last character of the string.
- */
-char *strnchrnul(const char *s, size_t count, int c);
-/**
- * strrchr - Find the last occurrence of a character in a string
- * @s: The string to be searched
- * @c: The character to search for
- */
-char *strrchr(const char *s, int c);
-/**
- * strnchr - Find a character in a length limited string
- * @s: The string to be searched
- * @count: The number of characters to be searched
- * @c: The character to search for
- *
- * Note that the %NUL-terminator is considered part of the string, and can
- * be searched for.
- */
-char *strnchr(const char *s, size_t count, int c);
-size_t strnlen(const char *s, size_t count);
-/**
- * strspn - Calculate the length of the initial substring of @s which only
- * contain letters in @accept
- * @s: The string to be searched
- * @accept: The string to search for
- */
-size_t strspn(const char *s, const char *accept);
-/**
- * strcspn - Calculate the length of the initial substring of @s which does not
- * contain letters in @reject
- * @s: The string to be searched
- * @reject: The string to avoid
- */
-size_t strcspn(const char *s, const char *reject);
-/**
- * strpbrk - Find the first occurrence of a set of characters
- * @cs: The string to be searched
- * @ct: The characters to search for
- */
-char *strpbrk(const char *cs, const char *ct);
-/**
- * strsep - Split a string into tokens
- * @s: The string to be searched
- * @ct: The characters to search for
- *
- * strsep() updates @s to point after the token, ready for the next call.
- *
- * It returns empty tokens, too, behaving exactly like the libc function
- * of that name. In fact, it was stolen from glibc2 and de-fancy-fied.
- * Same semantics, slimmer shape. ;)
- */
-char *strsep(char **s, const char *ct);
-/**
- * memset - Fill a region of memory with the given value
- * @s: Pointer to the start of the area.
- * @c: The byte to fill the area with
- * @count: The size of the area.
- *
- * Do not use memset() to access IO space, use memset_io() instead.
- */
-void *memset(void *s, int c, size_t count);
-/**
- * memset16() - Fill a memory area with a uint16_t
- * @s: Pointer to the start of the area.
- * @v: The value to fill the area with
- * @count: The number of values to store
- *
- * Differs from memset() in that it fills with a uint16_t instead
- * of a byte.  Remember that @count is the number of uint16_ts to
- * store, not the number of bytes.
- */
-void *memset16(uint16_t *s, uint16_t v, size_t count);
-/**
- * memset32() - Fill a memory area with a uint32_t
- * @s: Pointer to the start of the area.
- * @v: The value to fill the area with
- * @count: The number of values to store
- *
- * Differs from memset() in that it fills with a uint32_t instead
- * of a byte.  Remember that @count is the number of uint32_ts to
- * store, not the number of bytes.
- */
-void *memset32(uint32_t *s, uint32_t v, size_t count);
-/**
- * memset64() - Fill a memory area with a uint64_t
- * @s: Pointer to the start of the area.
- * @v: The value to fill the area with
- * @count: The number of values to store
- *
- * Differs from memset() in that it fills with a uint64_t instead
- * of a byte.  Remember that @count is the number of uint64_ts to
- * store, not the number of bytes.
- */
-void *memset64(uint64_t *s, uint64_t v, size_t count);
-/**
- * memmove - Copy one area of memory to another
- * @dest: Where to copy to
- * @src: Where to copy from
- * @count: The size of the area.
- *
- * Unlike memcpy(), memmove() copes with overlapping areas.
- */
-void *memmove(void *dest, const void *src, size_t count);
-/**
- * memcmp - Compare two areas of memory
- * @cs: One area of memory
- * @ct: Another area of memory
- * @count: The size of the area.
- */
-int memcmp(const void *cs, const void *ct, size_t count);
-/**
- * bcmp - returns 0 if and only if the buffers have identical contents.
- * @a: pointer to first buffer.
- * @b: pointer to second buffer.
- * @len: size of buffers.
- *
- * The sign or magnitude of a non-zero return value has no particular
- * meaning, and architectures may implement their own more efficient bcmp(). So
- * while this particular implementation is a simple (tail) call to memcmp, do
- * not rely on anything but whether the return value is zero or non-zero.
- */
-int bcmp(const void *a, const void *b, size_t len);
+void *memchr(const void *str, int c, size_t n);
 
 /**
- * memscan - Find a character in an area of memory.
- * @addr: The memory area
- * @c: The byte to search for
- * @size: The size of the area.
- *
- * returns the address of the first occurrence of @c, or 1 byte past
- * the area if @c is not found
+ * Compares the first n bytes of str1 and str2.
  */
-void *memscan(void *addr, int c, size_t size);
+int memcmp(const void *str1, const void *str2, size_t n);
+
 /**
- * strstr - Find the first substring in a %NUL terminated string
- * @s1: The string to be searched
- * @s2: The string to search for
+ * Copies n characters from src to dest.
  */
-char *strstr(const char *s1, const char *s2);
+void *memcpy(void *dest, const void *src, size_t n);
+
 /**
- * strnstr - Find the first substring in a length-limited string
- * @s1: The string to be searched
- * @s2: The string to search for
- * @len: the maximum number of characters to search
+ * Copies the character c (an unsigned char) to the first n characters of the
+ * string pointed to, by the argument str.
  */
-char *strnstr(const char *s1, const char *s2, size_t len);
+void *memset(void *str, int c, size_t n);
+
 /**
- * memchr - Find a character in an area of memory.
- * @s: The memory area
- * @c: The byte to search for
- * @n: The size of the area.
- *
- * returns the address of the first occurrence of @c, or %NULL
- * if @c is not found
+ * Appends the string pointed to, by src to the end of the string pointed to, by
+ * dest up to n characters long.
  */
-void *memchr(const void *s, int c, size_t n);
+char *strncat(char *dest, const char *src, size_t n);
+
+/**
+ * Searches for the first occurrence of the character c (an unsigned char) in
+ * the string pointed to, by the argument str.
+ */
+char *strchr(const char *str, int c);
+
+/**
+ * Compares at most the first n bytes of str1 and str2.
+ */
+int strncmp(const char *str1, const char *str2, size_t n);
+
+/**
+ * Copies up to n characters from the string pointed to, by src to dest.
+ */
+char *strncpy(char *dest, const char *src, size_t n);
+
+/**
+ * Calculates the length of the initial segment of str1 which consists entirely
+ * of characters not in str2.
+ */
+size_t strcspn(const char *str1, const char *str2);
+
+/**
+ * Computes the length of the string str up to but not including the terminating
+ * null character.
+ */
+size_t strlen(const char *str);
+
+/**
+ * Finds the first character in the string str1 that matches any character
+ * specified in str2.
+ */
+char *strpbrk(const char *str1, const char *str2);
+
+/**
+ * Searches for the last occurrence of the character c (an unsigned char) in the
+ * string pointed to by the argument str.
+ */
+char *strrchr(const char *str, int c);
+
+/**
+ * Finds the first occurrence of the entire string needle (not including the
+ * terminating null character) which appears in the string haystack.
+ */
+char *strstr(const char *haystack, const char *needle);
+
+/**
+ * Breaks string str into a series of tokens separated by delim.
+ */
+char *strtok(char *str, const char *delim);
 
 
 #endif/* FB7EC850_A287_4745_A5C1_2DA599F2D018 */
